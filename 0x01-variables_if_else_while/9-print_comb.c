5fd49ae63c8006c2a06ee8a3c27690045c2df44c
Sohilahamdy <sohila.fathy22@gmail.com>
2023-04-21 21:53:26 -0700

    finishing-3

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: print all alphabet letters
+ *
+ * Return: 0 (success)
+*/
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + 48);
+		if (digit != 9)
+		{
+			putchar(' ');
+			putchar('`');
+		}
+		digit++;
+	}
+	putchar('\n');
+	return (0);
+}
