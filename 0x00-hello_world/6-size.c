2e5e0261bf345db4678123003eefb4005e3a5ff9
Sohilahamdy <sohila.fathy22@gmail.com>
2023-09-11 16:59:04 -0700

    0x00. C - Hello, World

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	long long int d;
 	float f;
 
-	ptintf("Size of a char: %lu byte(s)", (unsigned long)sizeof(a));
-	ptintf("Size of an int: %lu byte(s)", (unsigned long)sizeof(b));
-	ptintf("Size of a long int: %lu byte(s)", (unsigned long)sizeof(c));
-	ptintf("Size of a long long int: %lu byte(s)", (unsigned long)sizeof(d));
-	ptintf("Size of a float: %lu byte(s)", (unsigned long)sizeof(f));
+	printf("Size of a char: %lu byte(s)", (unsigned long)sizeof(a));
+	printf("Size of an int: %lu byte(s)", (unsigned long)sizeof(b));
+	printf("Size of a long int: %lu byte(s)", (unsigned long)sizeof(c));
+	printf("Size of a long long int: %lu byte(s)", (unsigned long)sizeof(d));
+	printf("Size of a float: %lu byte(s)", (unsigned long)sizeof(f));
 	return (0);
 }
