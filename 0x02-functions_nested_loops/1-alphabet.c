d1d0a4f032561cdcaab5d0c186f0d34f5df824ab
Sohilahamdy <sohila.fathy22@gmail.com>
2023-09-12 06:50:24 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - a function that prints the alphabet, in lowercase
+ *
+ * Return: 0 (success)
+ */
+
+void main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
+	_putchar('\n');
+
+}
