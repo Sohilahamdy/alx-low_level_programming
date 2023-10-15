be97b3135621d62fae41a19db7875c7902e118bd
Sohilahamdy <sohila.fathy22@gmail.com>
2023-10-15 02:52:43 -0700

    0x0F-function_pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,12 +2,12 @@
 #include <stdlib.h>
 
 /**
- * *main -  program that prints the opcodes of its own main function.
- * *@argc: number of arguments passed to the program
- * *@argv: array of arguments
- * *
- * *Return: on success, 1 or 2 in case of failure
- * */
+ * main -  program that prints the opcodes of its own main function.
+ * @argc: number of arguments passed to the program
+ * @argv: array of arguments
+ *
+ * Return: on success, 1 or 2 in case of failure
+ */
 
 int main(int argc, char *argv[])
 {
