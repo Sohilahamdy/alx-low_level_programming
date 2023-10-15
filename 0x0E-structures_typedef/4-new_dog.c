2b9d26116b92a7496c06f278895a6d9637be13f3
Sohilahamdy <sohila.fathy22@gmail.com>
2023-10-15 07:40:19 -0700

    0x0E-structures_typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -7,10 +7,11 @@
  * *a string
  * *@str: string to be counted
  * *Return: returns length of string
- * */
+ */
 int _strlen(char *str)
 {
 	int len = 0;
+
 	while (str)
 		len++;
 
@@ -24,7 +25,7 @@ int _strlen(char *str)
  * *@dest:buffer storing string copy
  * *@src: buffer storing string to copy
  * *Return:returns copied string
- * */
+ */
 char *_strcopy(char *dest, char *src)
 {
 	int index = 0;
@@ -46,7 +47,7 @@ char *_strcopy(char *dest, char *src)
  * *@owner: owner of new dog
  * *Return: returns NULL in case
  * *of failure
- * */
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
